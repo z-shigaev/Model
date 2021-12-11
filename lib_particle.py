dd9b29a97e4d22b838279b50e64510c38de21f06
Shigaev Zamir <z.shigaev@g.nsu.ru>
2021-12-11 16:17:39 +0700

    test force

--- lib_particle.py
+++ lib_particle.py
@@ -1,4 +1,5 @@
 import math
+import matplotlib.pyplot as plt
 
 class Particle:
     def __init__(self, weight, charge, coords, velocity):
@@ -7,24 +8,44 @@ class Particle:
         self.coords = coords
         self.velocity = velocity
 
-    def change_moving(self, power, time):
-        x = (power[0]*math.pow(time, 2))/(2*self.weight)+velocity[0]*time+coords[0]
-        y = (power[1]*math.pow(time, 2))/(2*self.weight)+velocity[1]*time+coords[1]
-        z = (power[2]*math.pow(time, 2))/(2*self.weight)+velocity[2]*time+coords[2]
-        vx = (power[0] * time) / self.weight + self.velocity[0]
-        vy = (power[1] * time) / self.weight + self.velocity[1]
-        vz = (power[2] * time)  /self.weight + self.velocity[2]
+    def change_moving(self, force, time):
+        x = (force[0]*math.pow(time, 2))/(2*self.weight)+self.velocity[0]*time+self.coords[0]
+        y = (force[1]*math.pow(time, 2))/(2*self.weight)+self.velocity[1]*time+self.coords[1]
+        z = (force[2]*math.pow(time, 2))/(2*self.weight)+self.velocity[2]*time+self.coords[2]
+        vx = (force[0] * time) / self.weight + self.velocity[0]
+        vy = (force[1] * time) / self.weight + self.velocity[1]
+        vz = (force[2] * time) / self.weight + self.velocity[2]
         self.coords = [x, y, z]
         self.velocity = [vx, vy, vz]
         return x, y, z
+
+    def get_status(self):
+        coords = self.coords
+        vel = self.velocity
+        print("Coords: {0}".format(coords))
+        print("Velocity: {0}".format(vel))
 
 
 
 
 if __name__ == "__main__":
-    coords = [1, 1, 1]
-    velocity = [0, 0, 0]
-    particle = Particle(1, 1, coords, velocity)
+    coords = [0, 0, 0]
+    velocity = [100, 100, 0]
+    particle = Particle(0.1, 0, coords, velocity)
+    track_x = []
+    track_y = []
+    force = [0, -10, 0]
+    time = 0.1
+    track_x.append(coords[0])
+    track_y.append(coords[1])
+    for i in range(0, 20):
+        x, y, z = particle.change_moving(force, time)
+        track_x.append(x)
+        track_y.append(y)
+        # particle.get_status()
+    plt.plot(track_x, track_y)
+    plt.show()
+
 
 
 
