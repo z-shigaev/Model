77e889a34dd1ba0c8de31bfcb073b574a91e9189
Shigaev Zamir <z.shigaev@g.nsu.ru>
2021-12-18 16:09:40 +0700

    Problems

--- electric_field.py
+++ electric_field.py
@@ -4,6 +4,6 @@ import math
 def get_field(coords, coeff):
     mod = math.pow(coords[0], 2) + math.pow(coords[1], 2)
     r = math.pow(mod, 0.5)
-    ex = (coeff/math.pow(r, 2))*(coords[0]/mod)
-    ey = (coeff/math.pow(r, 2))*(coords[1]/mod)
+    ex = (coeff/r)*(coords[0]/r)
+    ey = (coeff/r)*(coords[1]/r)
     return ex, ey, 0
--- lib_force.py
+++ lib_force.py
@@ -9,14 +9,19 @@ def force_electric(electric_field, charge):
 
 
 def force_magnet(magnet_field, velocity, charge):
-    # print(charge*np.cross(velocity, magnet_field))
-    return charge*np.cross(velocity, magnet_field)
+    fx = (velocity[1]*magnet_field[2]-velocity[2]*magnet_field[1])
+    fy = (velocity[2]*magnet_field[0]-velocity[0]*magnet_field[2])
+    fz = (velocity[0]*magnet_field[1]-velocity[1]*magnet_field[0])
+    return fx, fy, fz
+    # return charge*np.cross(velocity, magnet_field)
 
 
 def force_gravity(AoFF, weight):
     # AoFF - Acceleration of Free Fall
     fx = weight*AoFF[0]
     fy = weight*AoFF[1]
+    # print('force')
+    # print(fx, fy)
     fz = weight*AoFF[2]
     return np.array([fx, fy, fz])
 
